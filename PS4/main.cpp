8cad515e861e48d49fea35ab54255cd661048a8d
Adil Khan <adiliitk2k16@gmal.com>
2023-07-16 23:04:36 +0530

    PS4 and PS5

---
+++ PS4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+#include "PS4.h"
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
---
+++ PS5/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+#include "PS5.h"
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
