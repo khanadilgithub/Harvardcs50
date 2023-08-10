ffbaace8258dd4780eb957a35d26982ecaaaebeb
Adil Khan <adiliitk2k16@gmal.com>
2023-08-10 22:32:38 +0530

    Cash

--- PS6/cash.py
+++ PS6/cash.py
@@ -1,2 +1,20 @@
-#wirte the cash progaram in this file
-print("This is cash progarm file")
+
+change=-1.0;
+while(change<0):
+    try :
+        change = float(input("Changed Owed: "))
+
+    except ValueError:
+        change=-1.0
+    
+change*=100
+
+quater=int(change/25)
+dime=int((change%25)/10)
+nickels=int(((change%25)%10)/5)
+pennies=int((((change%25)%10)%5))
+
+totalCoins=quater+dime+nickels+pennies
+
+print(totalCoins)
+
