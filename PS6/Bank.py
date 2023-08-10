ade2d5acb85617915c12b17589bf9d5400c3aa43
Adil Khan <adiliitk2k16@gmal.com>
2023-08-10 21:43:40 +0530

    Bank updated code

--- PS6/Bank.py
+++ PS6/Bank.py
@@ -1,8 +1,9 @@
 greeting = input("Greeting: ")
-greeting = greeting.lower()
-if(greeting=="hello"):
+greeting = (greeting.lower()).strip()
+
+if(greeting.startswith("hello")):
     print("$0")
-elif(greeting[0]=='h'):
+elif(greeting.startswith('h')):
     print("$20")
 else:
     print("$100")
